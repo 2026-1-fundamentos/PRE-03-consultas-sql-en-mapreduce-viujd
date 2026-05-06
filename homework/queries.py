a0b274852c0a5ddb54733dd22a0284302b74ab16
Juan David Robayo <141962352+viujd@users.noreply.github.com>
2026-05-06 09:48:12 -0500

    Update queries.py

--- homework/queries.py
+++ homework/queries.py
@@ -1,8 +1,7 @@
 # pylint: disable=broad-exception-raised
 # pylint: disable=import-error
 
-
-import .mapreduce.run_mapreduce_job as run_mapreduce_job  # type: ignore
+from .mapreduce import hadoop as run_mapreduce_job  # type: ignore
 
 #
 # Columns:
@@ -134,35 +133,7 @@ def reducer_query_5(sequence):
 # SELECT day, AVG(tip)
 # FROM tips
 # GROUP BY day;
-<<<<<<< HEAD
 #
-=======
-
-def mapper_query_6(sequence):
-    """Mapper"""
-    result = []
-    for index, (_, row) in enumerate(sequence):
-        if index == 0:
-            continue
-        row_values = row.strip().split(",")
-        day = row_values[3]
-        tip = float(row_values[1])
-        result.append((day, tip))
-    return result
-
-
-def reducer_query_6(sequence):
-    """Reducer"""
-    totals = dict()
-    counts = dict()
-    for key, value in sequence:
-        if key not in totals:
-            totals[key] = 0.0
-            counts[key] = 0
-        totals[key] += value
-        counts[key] += 1
-    return [(day, totals[day] / counts[day]) for day in totals]
->>>>>>> 3e46c49dd78d360f23515bb04f7b9d24f3c4c550
 
 
 #
@@ -172,56 +143,41 @@ def run():
     """Orquestador"""
 
     run_mapreduce_job(
-        mapper=mapper_query_1,
-        reducer=reducer_query_1,
-        input_directory="files/input",
-        output_directory="files/query_1",
+        mapper_fn=mapper_query_1,
+        reducer_fn=reducer_query_1,
+        input_folder="files/input/",
+        output_folder="files/query_1/",
     )
 
     run_mapreduce_job(
-        mapper=mapper_query_2,
-        reducer=reducer_query_2,
-        input_directory="files/input",
-        output_directory="files/query_2",
+        mapper_fn=mapper_query_2,
+        reducer_fn=reducer_query_2,
+        input_folder="files/input/",
+        output_folder="files/query_2/",
     )
 
     run_mapreduce_job(
-        mapper=mapper_query_3,
-        reducer=reducer_query_3,
-        input_directory="files/input",
-        output_directory="files/query_3",
+        mapper_fn=mapper_query_3,
+        reducer_fn=reducer_query_3,
+        input_folder="files/input/",
+        output_folder="files/query_3/",
     )
 
     run_mapreduce_job(
-        mapper=mapper_query_4,
-        reducer=reducer_query_4,
-        input_directory="files/input",
-        output_directory="files/query_4",
+        mapper_fn=mapper_query_4,
+        reducer_fn=reducer_query_4,
+        input_folder="files/input/",
+        output_folder="files/query_4/",
     )
 
     run_mapreduce_job(
-        mapper=mapper_query_5,
-        reducer=reducer_query_5,
-        input_directory="files/input",
-        output_directory="files/query_5",
+        mapper_fn=mapper_query_5,
+        reducer_fn=reducer_query_5,
+        input_folder="files/input/",
+        output_folder="files/query_5/",
     )
-<<<<<<< HEAD
-=======
 
-    run_mapreduce_job(
-    mapper=mapper_query_6,
-    reducer=reducer_query_6,
-    input_directory="files/input",
-    output_directory="files/query_6",
-    )
->>>>>>> 3e46c49dd78d360f23515bb04f7b9d24f3c4c550
 
 if __name__ == "__main__":
 
-
-
-<<<<<<< HEAD
     run()
-=======
-    run()
->>>>>>> 3e46c49dd78d360f23515bb04f7b9d24f3c4c550
